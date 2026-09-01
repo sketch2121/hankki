211478e0eb0b9a8749707c347b8c0836931584d1
sketch2121 <soleilim21@gmail.com>
2026-09-01 21:49:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hankki-v3";
+const CACHE = "hankki-v4";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
